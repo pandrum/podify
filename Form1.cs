8c9404b30d03d3d7ec70e2ea28b02871106997f6
pandrum <simon.bernsdorff@outlook.com>
2020-10-21 11:54:22 +0200

    test

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace AutomateEverything
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
