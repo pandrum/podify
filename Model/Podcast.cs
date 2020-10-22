1f81a500c4252c6ab346ba59e1ade2400204960b
kevinhansen99 <kevhan991203@student.jenseneducation.se>
2020-10-22 10:13:00 +0200

    Podcast klass

--- Model/Podcast.cs
+++ Model/Podcast.cs
@@ -4,5 +4,21 @@ namespace Model
 {
     public class Podcast
     {
+        public string URL { get; set; }
+        public string Name { get; set; }
+        public  string Category { get; set; }
+        public int Interval { get; set; }
+        public int TotalOfEpisodes { get; set; }
+
+        public Podcast (string aUrl, string aName, string aCategory, int aInterval, int aTotalOfEpisodes)
+
+        {
+            URL = aUrl;
+            Name = aName;
+            Category = aCategory;
+            Interval = aInterval;
+            TotalOfEpisodes = aTotalOfEpisodes;
+        }
+
     }
 }
