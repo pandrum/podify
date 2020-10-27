ae2fd6aa01ea490670a3626d99fe7e72f3663af6
kevinhansen99 <kevhan991203@student.jenseneducation.se>
2020-10-27 15:40:57 +0100

    Category

---
+++ DL/Repositories/CategoryRepository.cs
@@ -0,0 +1,41 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DL.Repositories
+{
+    class CategoryRepository : Category
+    {
+        public List<Category> listOfCategory;
+        public DataManager dataManager;
+
+        public void Create(Category entity)
+        {
+            listOfCategory.Add(entity);
+        }
+
+        public void Delete(Category index)
+        {
+            if (Name.Equals(index))
+            {
+                listOfCategory.Remove(index);
+            }
+
+        }
+
+        public void GetAllCategory()
+        {
+            foreach (var category in listOfCategory)
+            {
+                Console.WriteLine(listOfCategory);
+            }
+        }
+
+        public void SaveCategoryChanges()
+        {
+
+        }
+    }
+
+}
