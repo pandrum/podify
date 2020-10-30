ac0f303c5e5eed408832cd67449327a3206ff072
kevinhansen99 <kevhan991203@student.jenseneducation.se>
2020-10-30 10:32:29 +0100

    gfdg

--- DL/Validator.cs
+++ DL/Validator.cs
@@ -1,15 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Forms;
 
 namespace DL
 {
     public class Validator
     {
-        // Kolla om url är korrekt ifylld 
-        
+        //private PodcastController podcastController;
+        //public EpisodeController episodeController = new EpisodeController();
+       //public CategoryController categoryController = new CategoryController();
 
-        // Kolla om url har tilldelats ett namn 
+        public Validator ()
+        {
+           // podcastController = new PodcastController();
+
+        }
+        public bool IntervalHasValue(ComboBox interval)
+        {   
+            bool isValid = true;
+            if (interval.SelectedItem == null)
+            {
+
+                MessageBox.Show("You must choose an interval!");
+                isValid = false;
+            }
+
+            return isValid;
+
+
+            // Kolla om url är korrekt ifylld 
+
+
+            // Kolla om url har tilldelats ett namn 
+
+        }
 
     }
 }
