150f90b99797e07ea56549d2d2a11bf10e98ca02
Malin <Malin@DATOR-PC>
2020-10-30 09:49:29 +0100

    Validator

---
+++ DL/Validator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DL
+{
+    public class Validator
+    {
+        // Kolla om url är korrekt ifylld 
+        
+
+        // Kolla om url har tilldelats ett namn 
+
+    }
+}
